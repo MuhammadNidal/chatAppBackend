2cc3753bdb46c7e810265a97b466928e34e7f298
MuhammadNidal <mnidal414@gmail.com>
2025-10-19 15:44:32 +0500

    chat api backend middleware remove testing purpose

--- routes/messages.js
+++ routes/messages.js
@@ -1,10 +1,9 @@
 const controller = require('../controller/message.controller');
 const router = require('express').Router();
-const auth = require('../middleware/auth');
 
 
-router.get('/allMessages', auth, controller.getAllMessages);
-router.get('/:chatId', auth, controller.getMessagesByChatId);
-router.post('/sendmessage', auth, controller.addSendMessage);
+router.get('/allMessages',  controller.getAllMessages);
+router.get('/:chatId',  controller.getMessagesByChatId);
+router.post('/sendmessage',  controller.addSendMessage);
 
 module.exports = router;
